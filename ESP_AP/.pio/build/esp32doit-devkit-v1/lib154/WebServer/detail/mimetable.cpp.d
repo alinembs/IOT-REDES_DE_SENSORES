5a50d74c8ce90827110278be337b327051bce6a6
Aline Mariana <56563939+alinembs@users.noreply.github.com>
2023-06-02 15:32:37 -0300

    Delete ESP_AP directory

--- ESP_AP/.pio/build/esp32doit-devkit-v1/lib154/WebServer/detail/mimetable.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32doit-devkit-v1/lib154/WebServer/detail/mimetable.cpp.o: \
- /home/aline/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
- /home/aline/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
- /home/aline/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
--- ESP_AP/.pio/build/esp32doit-devkit-v1/liba76/SD/sd_diskio_crc.c.d
+++
@@ -1,2 +0,0 @@
-.pio/build/esp32doit-devkit-v1/liba76/SD/sd_diskio_crc.c.o: \
- /home/aline/.platformio/packages/framework-arduinoespressif32/libraries/SD/src/sd_diskio_crc.c
